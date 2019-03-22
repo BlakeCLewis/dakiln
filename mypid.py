11a65df5fb21744ad15aeb18a5658b2a99f243df
Lewis, Blake C <lewisbc@umsystem.edu>
2019-03-22 00:56:35 -0500

    dunders and scaling

--- mypid.py
+++ mypid.py
@@ -1,141 +1,158 @@
 class mypid():
     def __init__(self,Cpv,kc=6.0,kp=2.5,ki=0.5,kd=12.5,
-                      win=30,imax=20,imin=-20,omax=100,omin=0):
-
-        self.Kc = kc
-        self.Kp = kp
-        self.Ki = ki
-        self.Kd = kd
+                      win=30,imin=-20,imax=20,omin=0,omax=100):
 
         #time period in seconds
-        self.tau = win
+        self._tau = win
 
-        #Iterm limits: (Imin <= Iterm <= Imax)
+        # to minimize math in the pid loop scale the constants
+
+        #Kc is the required output to maintain temp per 100C
+        self._Kc = kc / 100
+
+        #time is constant, unless we change it
+        self._Kp = kp * 60 / win 
+        self._Ki = ki * 60 / win
+        self._Kd = kd * 60 / win
+
+        #_Iterm limits: (_Imin <= _Iterm <= _Imax)
         #windup remediation
-        self.Imax = imax
-        self.Imin = imin
-        self.Iterm = 0
+        self._Imax = imax
+        self._Imin = imin
+        self._Iterm = 0
 
-        #output limit (Omin <= output <= Omax)
-        #I am using % of window, so (0,100)
-        self.Omin = omin
-        self.Omax = omax
+        #output limit (_Omin <= output <= _Omax)
+        #% of window, (0,100)
+        self._Omin = omin
+        self._Omax = omax
 
         #pervious error to compute Dterm
-        self.laster = 0
+        self._laster = 0
 
         #what gets returned in manual mode
         #auto mode sets output so when switched to manual it will continue
-        self.output = 0
+        self._output = 0
 
         # false for manual overide mode
-        self.auto = True
+        self._auto = True
 
     def pid(self, Csp, Cpv, Cex):
 
         if self.auto :
             er = Csp-Cpv
          
-            #steady state loss (inside C - outide C) * Kc, Cterm ~= 6/100C
-            Cterm = self.Kc * (Cpv-Cex) / 100
+            #steady state loss (inside C - outide C) * _Kc
+            # heat loss is proportional to temp differitial
+            Cterm = self._Kc * (Cpv-Cex)
          
             # P time desired change
-            Pterm = self.Kp * er * 60 / self.tau
+            # to enable change in tau without blowing chunks
+            # 'delta t' need to be include in the calculation
+            Pterm = self._Kp * er
          
-            #add last error rate * Ki to the sum
-            self.Iterm += self.Ki * er * 60 / self.tau
-            if self.Iterm < self.Imin:
-                self.Iterm = self.Imin
-            elif self.Iterm > self.Imax:
-                self.Iterm = self.Imax
+            #add last error rate * _Ki to the sum
+            self._Iterm += self._Ki * er
+            if self._Iterm < self._Imin:
+                self._Iterm = self._Imin
+            elif self._Iterm > self._Imax:
+                self._Iterm = self._Imax
          
-            #(delta error rate) * Kd
-            Dterm = self.Kd * (er - laster) * 60 / self.tau
-            self.laster = er
+            #(delta error rate) * _Kd
+            Dterm = self._Kd * (er - _laster)
+            self._laster = er
          
-            op = (Cterm + Pterm + self.Iterm + Dterm)
+            op = (Cterm + Pterm + self._Iterm + Dterm)
          
-            if op < Omin:
-                op = Omin
-            elif op > self.Omax:
-                op = self.Omax
+            if op < _Omin:
+                op = _Omin
+            elif op > self._Omax:
+                op = self._Omax
 
-            self.output = op
-            #dataout(Cterm,Pterm,self.Iterm,Dterm,op)
+            self._output = op
+            #dataout(Cterm,Pterm,self._Iterm,Dterm,op)
 
             print(str(Cterm) +'+'+ str(Pterm) +
-                 '+'+ str(self.Iterm) +'+'+ str(Dterm) +'='+ str(op))
+                 '+'+ str(self._Iterm) +'+'+ str(Dterm) +'='+ str(op))
         else
-            print(str(self.output)) 
-
-        return(self.output/100 * self.tau)
+            print(str(self._output))
+            #if in manual mode do nothing and return unchange output
+        return(self._output * self._tau /100)
 
     #setters
     def auto(Cpv, Cex):
-        self.Iterm = self.output - (self.Kc * (Cpv - Cex))
-        self.laster = 0
+        """ switch to auto in a smooth operation
+            set Iterm to maintain current output
+            zero laster so Dterm does not contribute
+            requires internal and external temps
+        """
+        self._Iterm = self._output - (self._Kc * (Cpv - Cex))
+        self._laster = 0
         self.auto=True
 
-    def manual():
-        self.auto=False
+    def manual(op):
+        """"pause pid calculation
+            set output = op
+        """
+        self._output = op
+        self.auto = False
 
-    def setKc(self, Kacie):
-        self.Kc=Kacie
+    def set_Kc(self, Kacie):
+        self._Kc = Kacie / 100
 
-    def setKp(self, Kpee):
-        self.Kp=Kpee
+    def set_Kp(self, Kpee):
+        self._Kp = Kpee * 60 / self.tau
 
-    def setKi(self, Kai):
-        self.Ki=Kai
+    def set_Ki(self, Kai):
+        self._Ki = Kai * 60 / self.tau
 
-    def setKd(self, Kadee):
-        self.Kd=Kadee
+    def set_Kd(self, Kadee):
+        self._Kd = Kadee * 60 / self.tau
 
-    def setImin(self, hymin):
-        self.Imin=hymin
+    def set_Imin(self, eyemin):
+        self._Imin = eyemin
 
-    def setImax(self, eyemax):
-        self.Imax=eyemax
+    def set_Imax(self, eyemax):
+        self._Imax = eyemax
 
-    def setOmax(self, ohmin):
-        self.Omin=ohmin
+    def set_Omin(self, ohmin):
+        self._Omin = ohmin
 
-    def setOmax(self, ohmax):
-        self.Omax=ohmax
+    def set_Omax(self, ohmax):
+        self._Omax = ohmax
 
-    def settau(self, bod):
-        self.tau=bod
+    def set_tau(self, bod):
+        self._Kp *= self._tau / bod
+        self._Ki *= self._tau / bod
+        self._Kd *= self._tau / bod
+        self._tau = bod
 
-    def setI(self, eye):
-        self.Iterm = eye
+    #getters 
+    def get_Kc(self):
+        return self._Kc * 100
 
-    #getters
-    def getKc(self):
-        return self.Kc
+    def get_Kp(self):
+        return self._Kp * self._tau / 60
 
-    def getKp(self):
-        return self.Kp
+    def get_Ki(self):
+        return self._Ki * self._tau / 60
 
-    def getKi(self):
-        return self.Ki
+    def get_Kd(self):
+        return self._Kd * self._tau / 60
 
-    def getKd(self):
-        return self.Kd
+    def get_Imin(self):
+        return self._Imin
 
-    def getImin(self):
-        return self.Imin
+    def get_Imax(self):
+        return self._Imax
 
-    def getImax(self):
-        return self.Imax
+    def get_Omax(self):
+        return self._Omax
 
-    def getOmax(self):
-        return self.Omax
+    def get_tau(self):
+        return self._tau
 
-    def gettau(self):
-        return self.tau
+    def get_Iterm(self):
+        return self._Iterm
 
-    def getIterm(self):
-        return self.Iterm
-
-    def getauto(self):
-        return self.auto
+    def is_auto(self):
+        return self._auto
